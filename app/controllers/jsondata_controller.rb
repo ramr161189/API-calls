72df229b447a1f924545116cce1b70074ccb1ce5
ramr161189 <85715211+ramr161189@users.noreply.github.com>
2021-07-08 15:14:56 +0530

    Update jsondata_controller.rb

--- app/controllers/jsondata_controller.rb
+++ app/controllers/jsondata_controller.rb
@@ -119,37 +119,33 @@ end
 	
 def relatedWords
   $wordcall=1
-		apigeneration=Apigeneration.new
-		key=String(params[:key])
-		keyval=key[1,key.length]
-		wordparam=String(params[:word])
-		word=wordparam[1,key.length]
-		a=0
-		t=0
-		Apigeneration.find_each(:batch_size => 10000) do |apigenerations|
-			if apigenerations.apikey.chomp.casecmp(keyval.chomp)==0
-				val=Integer(apigenerations.usage)+1
-				apigenerations.update(usage: val)
-				a=1
-				Jsondatum.find_each(:batch_size => 10000) do |jsondata|
-					if jsondata.word==word
-						t=1
-						relatedWords=jsondata.relatedwords
-						$val="@relatedWords are #{relatedWords}@"
-						$jsonval=$val.to_json
-					end
-				end
-			end
-		end
-		if a==0
-			$val="Keynotfound"
-			
-		elsif t==0
-			$val="wordisnotpresent "
-		end
-		redirect_to '/words/word:{word}/relatedWords?api_key:{keyval}'
-
-	end
-
-
+  apigeneration=Apigeneration.new
+  key=String(params[:key])
+  keyval=key[1,key.length]
+  wordparam=String(params[:word])
+  word=wordparam[1,key.length]
+  a=0
+  t=0
+  Apigeneration.find_each(:batch_size => 10000) do |apigenerations|
+    if apigenerations.apikey.chomp.casecmp(keyval.chomp)==0
+      val=Integer(apigenerations.usage)+1
+      apigenerations.update(usage: val)
+      a=1
+      Jsondatum.find_each(:batch_size => 10000) do |jsondata|
+        if jsondata.word==word
+          t=1
+	  relatedWords=jsondata.relatedwords
+	  val="@relatedWords are #{relatedWords}@"
+	  $jsonval=val.to_json
+        end
+      end
+    end
+  end
+  if a==0
+    $val="Keynotfound"
+  elsif t==0
+    $val="wordisnotpresent "
+  end
+  redirect_to '/words/word:{word}/relatedWords?api_key:{keyval}'
+end
 end
