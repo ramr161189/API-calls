08b751ecf9a9c51d1ac013af8461738fef699805
ramr161189 <85715211+ramr161189@users.noreply.github.com>
2021-07-08 14:02:17 +0530

    Update apigeneration_controller.rb

--- app/controllers/apigeneration_controller.rb
+++ app/controllers/apigeneration_controller.rb
@@ -24,24 +24,22 @@ def delete
   redirect_to '/dashboard'
 end
 
-	def apikeycheck
-		apigeneration=Apigeneration.new
-		key=String(params[:key])
-		keyval=key[1,key.length]
-		t=0
-		Apigeneration.find_each(:batch_size => 10000) do |apigenerations|
-			if apigenerations.apikey.chomp.casecmp(keyval.chomp)==0
-				val=Integer(apigenerations.usage)+1
-				apigenerations.update(usage: val)
-				t=1
-				break
-			end
-		end
-		if(t==0)
-			$ram=0
-		else
-			$ram=100000
-		end
-		redirect_to '/dashboard'
-	end
+def apikeycheck
+  apigeneration = Apigeneration.new
+  key = String(params[:key])
+  keyval = key[1, key.length]
+  temp = 0
+  Apigeneration.find_each(:batch_size => 10000) do |apigenerations|
+    if apigenerations.apikey.chomp.casecmp(keyval.chomp) == 0
+      val = Integer(apigenerations.usage) + 1
+      apigenerations.update(usage: val)
+      temp = 1
+      break
+    end
+  end
+  if(temp == 0)
+   flash[:alert] = "API key is not present"
+  end
+  redirect_to '/dashboard'
+end
 end
