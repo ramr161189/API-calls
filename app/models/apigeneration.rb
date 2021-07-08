ba5e1f85a51bac63a9f358238cf7b1c5e84074a1
ramr161189 <85715211+ramr161189@users.noreply.github.com>
2021-07-08 15:23:33 +0530

    Update apigeneration.rb

--- app/models/apigeneration.rb
+++ app/models/apigeneration.rb
@@ -1,12 +1,2 @@
 class Apigeneration < ApplicationRecord
-	#has_many :apigenerations, through: :line_items
-	#require "securerandom"
-	#apigeneration=Apigeneration.new
-#	email=User.find(session[:user_id]).email
-	  #apigeneration.apikey=SecureRandom.uuid
-	  #apigeneration.usage=0
-	  #apigeneration.plan="basic"
-	  #apigeneration.email=$username
-	  #apigeneration.save
-
 end
