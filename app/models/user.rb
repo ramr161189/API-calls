f990b9df00dbbc7188379981c412c5ce00cda7b7
ramr161189 <85715211+ramr161189@users.noreply.github.com>
2021-07-08 15:25:09 +0530

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :comments
   EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
   validates :email, :password, :password_confirmation, presence: true
-   validates :email, :presence => true, :uniqueness => true
+  validates :email, :presence => true, :uniqueness => true
   has_secure_password
 end
