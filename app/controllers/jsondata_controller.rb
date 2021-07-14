4f9e3bc1bdeb75c627fee34dc601e386b67fea7a
ramr161189 <85715211+ramr161189@users.noreply.github.com>
2021-07-14 16:24:36 +0530

    Update jsondata_controller.rb

--- app/controllers/jsondata_controller.rb
+++ app/controllers/jsondata_controller.rb
@@ -2,14 +2,14 @@ class JsondataController < ApplicationController
   before_action :apikeycheck, only: [:randomWord, :definitions, :examples, :relatedwords]
   before_action :wordcheck, only: [:definitions, :examples, :relatedwords]
   def apikeycheck
-    if $totalapicalls<totalapis
-	    key = String(params[:key])
-	    $keyval = key[1,key.length]
-	    apigeneration = Apigeneration.find_by(apikey:$keyval)
-	    val = Integer(apigeneration.usage)+1
-	    apigeneration.update(usage: val)
+    if $totalapicalls < $apicalls
+      key = String(params[:key])
+      $keyval = key[1,key.length]
+      apigeneration = Apigeneration.find_by(apikey:$keyval)
+      val = Integer(apigeneration.usage)+1
+      apigeneration.update(usage: val)
     else
-	    format.html{redirect_to '/dashboard',notice: 'APIcalls Limit exceeded'}
+      format.html{redirect_to '/dashboard',notice: 'APIcalls Limit exceeded'}
   end
 
   def wordcheck
