5dffcd4863eb5d666b3f1d2683fab184e2548dc0
ramr161189 <85715211+ramr161189@users.noreply.github.com>
2021-07-15 10:19:24 +0530

    Update jsondata_controller.rb

--- app/controllers/jsondata_controller.rb
+++ app/controllers/jsondata_controller.rb
@@ -7,24 +7,28 @@ class JsondataController < ApplicationController
       $keyval = key[1,key.length]
       apigeneration = Apigeneration.find_by(apikey:$keyval)
       if apigeneration
-        val = Integer(apigeneration.usage)+1
+        val = Integer(apigeneration.usage) + 1
         apigeneration.update(usage: val)
-	@k=1
+	      user = User.find_by(email:$username)
+	      countval = Integer(user.count) + 1
+				user.update(count: countval)			
+	      @k=1
       else
         @k=0
       end      
     else
       format.html{redirect_to '/dashboard',notice: 'APIcalls Limit exceeded'}
+    end
   end
 
   def wordcheck
-    @w=0
-    if @k==1
+    @w = 0
+    if @k == 1
       wordparam = String(params[:word])
       @randomWord = wordparam[1,wordparam.length]
       jsondata = Jsondatum.find_by(word:$randomWord)
       if jsondata
-        @w=1	  
+        @w = 1	  
         @data = jsondata
       end
     else
@@ -58,7 +62,7 @@ class JsondataController < ApplicationController
   end
 
   def randomWord
-    if @k==1	  
+    if @k == 1	  
       id = rand 133..168
       word =  Jsondatum.find(id).word
       val = {"word" =>"#{word}"}
@@ -70,7 +74,7 @@ class JsondataController < ApplicationController
   end
 
   def definitions
-    if @w==1
+    if @w == 1
       definition = @data.definitions
       $jsonval = definition
     elsif @w==0 && @k==1
@@ -82,7 +86,7 @@ class JsondataController < ApplicationController
   end
 	
   def examples
-    if @w==1
+    if @w == 1
       examples = @data.examples
       $jsonval = examples
     elsif @w==0 && @k==1
@@ -94,7 +98,7 @@ class JsondataController < ApplicationController
   end
 
   def relatedWords
-   if @w==1
+   if @w == 1
       relatedWords = @data.relatedWords
       $jsonval = relatedWords
     elsif @w==0 && @k==1
