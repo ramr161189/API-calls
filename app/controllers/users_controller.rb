9369738e2410e5fd9db0074b2b5212ef28ef9744
ramr161189 <85715211+ramr161189@users.noreply.github.com>
2021-07-27 10:41:35 +0530

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,16 @@ def create
   user = User.new(user_params)
   if user.save
     session[:user_id] = user.id
+    if user.plan=='basic'
+      user.update_columns(maxkey:5)
+      user.update_columns(todaylimit:500)
+    elsif user.plan=='advance'
+      user.update_columns(maxkey:10)
+      user.update_columns(todaylimit:1000)
+    elsif user.plan=='expert'
+      user.update_columns(maxkey:20)
+      user.update_columns(todaylimit:2000)
+    end
     flash[:notice] = "created successfully"
     redirect_to '/'
    else
