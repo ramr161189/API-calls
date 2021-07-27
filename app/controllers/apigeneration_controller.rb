862b2db703089a3a7f7847d340c860add47526a6
ramr161189 <85715211+ramr161189@users.noreply.github.com>
2021-07-27 10:20:50 +0530

    Update apigeneration_controller.rb

--- app/controllers/apigeneration_controller.rb
+++ app/controllers/apigeneration_controller.rb
@@ -1,16 +1,24 @@
 class ApigenerationController < ApplicationController
-def add
-  apigeneration = Apigeneration.new
-  apigeneration.apikey = SecureRandom.uuid
-  apigeneration.usage = 0
-  apigeneration.email = $username
-  apigeneration.save
-  redirect_to '/dashboard'
-end
-	
-def delete
-  val = params[: my_params]
-  Apigeneration.where(apikey:val ).delete_all
-  redirect_to '/dashboard'
-end
+  def add
+    user=User.find(session[:user_id])
+    if user.apikeycount < user.maxkey
+      apigeneration = Apigeneration.new
+      apigeneration.apikey = SecureRandom.uuid
+      apigeneration.usage = 0
+      apigeneration.email = $username
+      user.update_columns(apikeycount:user.apikeycount+1)
+      apigeneration.save
+    else
+      flash[:notice]="maxkeysover"
+    end
+    redirect_to '/dashboard'
+  end
+		
+  def delete
+    val = params[:my_params]
+    Apigeneration.where(apikey:val ).delete_all
+    user=User.find(session[:user_id])
+    user.update_columns(apikeycount:user.apikeycount-1)
+    redirect_to '/dashboard'
+    end
 end
